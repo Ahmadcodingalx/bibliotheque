f80728fb4ebf32296db7aebf98a7c4f6e7e6be2f
= <=>
2024-06-14 12:01:32 +0000

    update-x

--- src/main/java/org/example/livres/Controllers/ConnectionController.java
+++ src/main/java/org/example/livres/Controllers/ConnectionController.java
@@ -58,7 +58,6 @@ public class ConnectionController implements Initializable {
         connection = database.connectDB();
 
         try {
-
             Alert alert;
 
             assert connection != null;
